0dfca2cb41ffc96a4b09de41c5c389a905d1ea0c
choisuyong44 <tlgmdtl1118@gmail.com>
2024-03-07 11:00:57 +0900

    create models

--- crud/__init__.py
+++ crud/__init__.py
@@ -17,6 +17,7 @@ def create_app():
     # ORM -> db, migarte init 
     db.init_app(app)
     migrate.init_app(app,db)
+    from . import models
 
     from .views import main_views
     app.register_blueprint(main_views.bp)
